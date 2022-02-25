f2dbb40da478ee83c216b69b802ea3386d2ae042
Salmi <juliansalm@gmx.de>
2022-02-25 11:37:19 +0100

    dbcon connection im LoginFrm Hinzugefügt

--- WinUSBAuthentication/FrmLogIn.cs
+++ WinUSBAuthentication/FrmLogIn.cs
@@ -98,7 +98,7 @@ namespace WinUSBAuthentication
 
         private void lregister_Click(object sender, EventArgs e)
         {
-            FrmCreateAcc frmca = new FrmCreateAcc();
+            FrmCreateAcc frmca = new FrmCreateAcc(dbcon);
 
             this.Hide();
             frmca.ShowDialog();
